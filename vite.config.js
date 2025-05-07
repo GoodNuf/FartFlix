234c787722b294762bfee6b35595f9fb3509f44a
GoodNuf <137121529+GoodNuf@users.noreply.github.com>
2025-05-07 16:57:49 -0400

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-// })
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Test/",
-};
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/Test/",
+// };
