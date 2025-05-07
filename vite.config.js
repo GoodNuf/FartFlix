792d984c5bb083a34bbe20edea0b690b230c6777
GoodNuf <137121529+GoodNuf@users.noreply.github.com>
2025-05-07 16:20:18 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-// vite.config.js
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
   base: '/Test/', // Replace with your GitHub repo name
   plugins: [vue()],
