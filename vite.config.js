fac9f796c5500b3f990155838f3c0d6d68ec089e
GoodNuf <137121529+GoodNuf@users.noreply.github.com>
2025-05-08 15:01:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
-  base: '/Test/', // Replace with your GitHub repo name
+  base: '/FartFlix/', // Replace with your GitHub repo name
   plugins: [vue()],
 })
