d8b56a1618647460a3df4da3938758340297b1bf
GoodNuf <137121529+GoodNuf@users.noreply.github.com>
2025-10-11 16:05:05 -0400

    status and catalog update

--- src/components/index.vue
+++ src/components/index.vue
@@ -94,8 +94,8 @@ const openShows = async () => {
       <PlansIcon />
     </template>
     <template #heading>Plans</template>
-    <h4 style="color:white;">FartFlix<span style="color:#69CCC9;">+</span> Monthly</h4>(Access to 1,200+ movies & 6,000+ episodes of TV + 1 free monthly request): <br>$<span style="color:#69CCC9;">6</span>/month<sup>1</sup></br>
-    <h4 style="color:white;">FartFlix<span style="color:#69CCC9;">+</span> Annual</h4>(Access to 1,200+ movies & 6,000+ episodes of TV + 1 free monthly request): <br>$<span style="color:#69CCC9;">55</span>/year<sup>1</sup>
+    <h4 style="color:white;">FartFlix<span style="color:#69CCC9;">+</span> Monthly</h4>(Access to 1,200+ movies & 14,000+ episodes of TV + 1 free monthly request): <br>$<span style="color:#69CCC9;">6</span>/month<sup>1</sup></br>
+    <h4 style="color:white;">FartFlix<span style="color:#69CCC9;">+</span> Annual</h4>(Access to 1,200+ movies & 14,000+ episodes of TV + 1 free monthly request): <br>$<span style="color:#69CCC9;">55</span>/year<sup>1</sup>
   </WelcomeItem>
   <WelcomeItem>
     <template #icon>
@@ -121,7 +121,7 @@ const openShows = async () => {
     <a href="https://watch.fartflix.com" target="_blank" rel="noopener">Start streaming</a><span> | </span>
     <a href="https://request.fartflix.com" target="_blank" rel="noopener">Make a request</a><span> | </span>
     <a href="#" @click.prevent="openClient" style="color: #69CCC9;">Clients</a><span> | </span>
-    <a href="#" @click.prevent="openStatus" style="color: #69CCC9;">Server status</a>
+    <a href="https://status.fartflix.com" target="_blank" rel="noopener">Server status</a>
   </WelcomeItem>
   <Terms>
     <template #txt>
